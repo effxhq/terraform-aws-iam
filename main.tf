4520c0270b4a2a8ae364c45c12e22fd1e254478a
Daniel McSheehy <dsm140130@utdallas.edu>
2021-04-29 12:57:19 -0700

    fix non snakecase

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
 resource "aws_iam_role" "effx_aws_ecs_integration_role" {
   name                = "effx-AWS-ECS-Integration-Role"
   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
-  managed_policy_arns = [aws_iam_policy.effx-AWS-ECS-Integration.arn]
+  managed_policy_arns = [aws_iam_policy.effx_aws_ecs_integration.arn]
 
   tags = var.resource_tags
 }
 
-resource "aws_iam_policy" "effx-AWS-ECS-Integration" {
+resource "aws_iam_policy" "effx_aws_ecs_integration" {
   name = "effx-AWS-ECS-Integration"
 
   policy = jsonencode({
